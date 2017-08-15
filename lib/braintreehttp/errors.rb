09fe9eeb7303cb8096b6001e0d29630a5efe98e8
sashaweiss and sdcoffey <code@getbraintree.com>
2017-08-15 00:00:04 +0000

    Change ServiceIOError to HttpError for consistency

--- lib/braintreehttp/errors.rb
+++ lib/braintreehttp/errors.rb
@@ -1,5 +1,5 @@
 module BraintreeHttp
-  class ServiceIOError < IOError
+  class HttpError < IOError
     attr_accessor :status_code, :result, :headers
     def initialize(status_code, result, headers)
       @status_code = status_code
