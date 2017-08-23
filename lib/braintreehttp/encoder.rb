69c58dfbf8ec057ca0cdd50b63bac55d5abd199d
sdcoffey <steve.coffey@getbraintree.com>
2017-08-23 23:30:37 +0000

    BTDXCLTS-814 HttpClient supports text/* content-types

--- lib/braintreehttp/encoder.rb
+++ lib/braintreehttp/encoder.rb
@@ -3,31 +3,72 @@ require 'json'
 module BraintreeHttp
 
   class Encoder
+    def initialize
+      @encoders = [Json.new, Text.new]
+    end
 
     def serialize_request(req)
       raise UnsupportedEncodingError.new('HttpRequest did not have Content-Type header set') unless req.headers && (req.headers['content-type'] || req.headers['Content-Type'])
 
       content_type = req.headers['content-type'] || req.headers['Content-Type']
-      raise UnsupportedEncodingError.new("Unable to serialize request with Content-Type #{content_type}. Supported encodings are #{supported_encodings}") unless content_type == 'application/json'
+      content_type = content_type.first if content_type.kind_of?(Array)
 
-      JSON.generate(req.body)
+      enc = _encoder(content_type)
+      raise UnsupportedEncodingError.new("Unable to serialize request with Content-Type #{content_type}. Supported encodings are #{supported_encodings}") unless enc
+
+      enc.encode(req.body)
     end
 
     def deserialize_response(resp, headers)
       raise UnsupportedEncodingError.new('HttpResponse did not have Content-Type header set') unless headers && (headers['content-type'] || headers['Content-Type'])
 
       content_type = headers['content-type'] || headers['Content-Type']
-      raise UnsupportedEncodingError.new("Unable to deserialize response with Content-Type #{content_type}. Supported decodings are #{supported_decodings}") unless content_type.include? 'application/json'
+      content_type = content_type.first if content_type.kind_of?(Array)
 
-      JSON.parse(resp)
+      enc = _encoder(content_type)
+      raise UnsupportedEncodingError.new("Unable to deserialize response with Content-Type #{content_type}. Supported decodings are #{supported_encodings}") unless enc
+
+      enc.decode(resp)
     end
 
     def supported_encodings
-      ['application/json']
+      @encoders.map { |enc| enc.content_type.inspect }
     end
 
-    def supported_decodings
-      ['application/json']
+    def _encoder(content_type)
+      idx = @encoders.index { |enc| enc.content_type.match(content_type) }
+
+      @encoders[idx] if idx
+    end
+  end
+
+  class Json
+
+    def encode(body)
+      JSON.generate(body)
+    end
+
+    def decode(body)
+      JSON.parse(body)
+    end
+
+    def content_type
+      /^application\/json$/
+    end
+  end
+
+  class Text
+
+    def encode(body)
+      body.to_s
+    end
+
+    def decode(body)
+      body.to_s
+    end
+
+    def content_type
+      /^text\/.*/
     end
   end
 end
--- spec/braintreehttp/encoder_spec.rb
+++ spec/braintreehttp/encoder_spec.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 describe Encoder do
 
   describe 'serialize_request' do
-    it 'serializes the request' do
+    it 'serializes the request when content-type == application/json' do
       req = OpenStruct.new({
         :headers  => {
           "content-type" => "application/json"
@@ -25,6 +25,17 @@ describe Encoder do
       expect(Encoder.new.serialize_request(req)).to eq(expected)
     end
 
+    it 'serializes the request when content-type == text/*' do
+      req = OpenStruct.new({
+        :headers  => {
+          "content-type" => "text/plain"
+        },
+        :body => "some text"
+      })
+
+      expect(Encoder.new.serialize_request(req)).to eq("some text")
+    end
+
     it 'throws when content-type is not application/json' do
       req = OpenStruct.new({
         :headers  => {
@@ -33,7 +44,7 @@ describe Encoder do
         :body => { :string => "value" }
       })
 
-      expect{Encoder.new.serialize_request(req)}.to raise_error(UnsupportedEncodingError, /Unable to serialize request with Content-Type .*\. Supported encodings are \["application\/json"\]/)
+      expect{Encoder.new.serialize_request(req)}.to raise_error(UnsupportedEncodingError, /Unable to serialize request with Content-Type .*\. Supported encodings are .*/)
     end
 
     it 'throws when headers undefined' do
@@ -52,7 +63,7 @@ describe Encoder do
         "content-type" => ["application/xml"]
       }
 
-      expect{Encoder.new.deserialize_response(body, headers)}.to raise_error(UnsupportedEncodingError, /Unable to deserialize response with Content-Type .*\. Supported decodings are \["application\/json"\]/)
+      expect{Encoder.new.deserialize_response(body, headers)}.to raise_error(UnsupportedEncodingError, /Unable to deserialize response with Content-Type .*\. Supported decodings are .*/)
     end
 
     it 'throws when headers undefined' do
@@ -61,7 +72,7 @@ describe Encoder do
       expect{Encoder.new.deserialize_response(body, nil)}.to raise_error(UnsupportedEncodingError, 'HttpResponse did not have Content-Type header set')
     end
 
-    it 'deserializes the response' do
+    it 'deserializes the response when content-type == application/json' do
       expected = {
         "string" => "value",
         "number" => 1.23,
@@ -77,6 +88,13 @@ describe Encoder do
       body = '{"string":"value","number":1.23,"bool":true,"array":["one","two","three"],"nested":{"nested_string":"nested_value","nested_array":[1,2,3]}}'
 
       expect(Encoder.new.deserialize_response(body, headers)).to eq(expected)
+    end
+
+    it 'deserializes the response when content-type == text/*' do
+      headers = {"content-type" => ["text/plain"]}
+      body = 'some text'
+
+      expect(Encoder.new.deserialize_response(body, headers)).to eq('some text')
     end
   end
 end
